a9ab0862e60b26605f8f82193f8e23b93976a493
Prodanoff <76869212+Prodanoff@users.noreply.github.com>
2021-04-05 08:55:21 +0300

    Automatic Year update
    Automatic Year update

--- spa/src/components/Footer/Footer.js
+++ spa/src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
 import './Footer.css'
+import { useState, useEffect } from 'react'
 const Footer = () => {
+    const [year, setYear] = useState(``)
+    useEffect(()=>{
+        let thisEyar= new Date()
+        
+        setYear(thisEyar.getFullYear())
+    }
+    )
     return (
         <footer className="tm-footer">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <p className="text-xs-center tm-footer-text">Copyright &copy; CRAFT MASTER Margarita Prodanova 2021 </p>
+                <p className="text-xs-center tm-footer-text">Copyright &copy; CRAFT MASTER Margarita Prodanova {year} </p>
             </div>
         </footer>
     )
